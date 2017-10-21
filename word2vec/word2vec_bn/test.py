3d8edadf94144235562195cb5371cb1ba8905032
Pritom Saha Akash <pritomsaha163@gmail.com>
2017-10-22 00:30:46 +0600

    train wiki_bn_11

--- word2vec/word2vec_bn/test.py
+++
@@ -1,1 +0,0 @@
-from gensim.models import Word2Vec
--- word2vec/word2vec_bn/train.py
+++ word2vec/word2vec_bn/train.py
@@ -1,50 +1,84 @@
-import pandas as pa
 import re
-import nltk.data
-from nltk.corpus import stopwords
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',level=logging.INFO)
 
 bn_char_pattern = re.compile(r'[^\u0980-\u0983\u0985-\u098C\u098F-\u0990\u0993-\u09A8\u09AA-\u09B0\u09B2\u09B6-\u09B9\u09BC-\u09C4\u09C7-\u09C8\u09CB-\u09CE\u09D7\u09DC-\u09DD\u09DF-\u09E3\u09F0-\u09FD]', re.UNICODE)
-
-def  get_wordlist(text, remove_stopwords = False):
+stopwords = []
+def  get_bn_wordlist(text, remove_stopwords = False):
 	text = re.sub(bn_char_pattern, ' ', text)
-	words = text.split()
+	words = text.strip().split(" ")
 
 	if remove_stopwords:
-		print("i will do that")
+		words = [w for w in words if w not in stopwords]
 
 	return (words)
 
-def  train(sentences):
-	import logging
+
+def  get_en_wordlist(text, remove_stopwords = False):
+	text = re.sub(r'[^a-zA-Z]', ' ', text)
+	words = text.strip().lower().split(" ")
+
+	if remove_stopwords:
+		stops = set(stopwords.words("english"))
+		words = [w for w in words if w not in stops]
+
+	return (words)
+
+def  train(sentences, model_name):
 	from gensim.models import word2vec
-	logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',level=logging.INFO)
 	
-	num_features = 300    # Word vector dimensionality                      
-	min_word_count = 40   # Minimum word count                        
+	num_features = 200   # Word vector dimensionality                      
+	min_word_count = 5   # Minimum word count                        
 	num_workers = 4       # Number of threads to run in parallel
-	context = 10          # Context window size                                                                                    
-	downsampling = 1e-3   # Downsample setting for frequent words
+	context = 5        # Context window size                                                                                    
+	# downsampling = 1e-3   # Downsample setting for frequent words
 
 	print("Training model.......")
 	model = word2vec.Word2Vec(sentences, workers=num_workers, \
             size=num_features, min_count = min_word_count, \
-            window = context, sample = downsampling)
+            window = context, sg = 0)
 
 	model.init_sims(replace = True)
-	model_name = "my_model"
 	model.save(model_name)
+
+# you can retrain model with more sentences
+def retrain(new_sentences):
+	from gensim.models import Word2Vec
+	model = Word2Vec.load('my_model')
+	model.build_vocab(new_sentences, update=True)
+	model.train(new_sentences, total_examples = model.corpus_count, epochs = model.iter)
+
+def visualize_model(model_name):
+	from gensim.models import Word2Vec
+	from sklearn.manifold import TSNE
+	import matplotlib.pyplot as plt
+	import pandas as pa
+
+	model = Word2Vec.load(model_name)
+	vocab = list(model.wv.vocab)
+	X = model[vocab]
+	
+	tsne = TSNE(n_components=2)
+	X_tsne = tsne.fit_transform(X)
+	df = pd.concat([pd.DataFrame(X_tsne),pd.Series(vocab)],axis=1)
+	df.columns = ['x', 'y', 'word']
+
+	print(df)
 
 
 def main():
-	bn_wiki_file = open('ben_wikipedia_2016_30K-sentences.txt', 'r')
 	sentences = []
+	stopwords = open('bn_stopwords.txt', 'r').read().split(',')
+	bn_wiki_file = open('ben_wikipedia_2011_100K-sentences.txt', 'r')
 	lines = bn_wiki_file.readlines()
 	
 	for line in lines:
-		sentences.append(get_wordlist(line))
+		sentences.append(get_bn_wordlist(line, True))
 
-	train(sentences)
-
+	train(sentences, "bn_model_sg0")
 
 if __name__ == '__main__':
-	main()
+	# main()
+
+	visualize_model("bn_model_sg0")
+	
